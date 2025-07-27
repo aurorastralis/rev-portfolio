12594f839d63be993b54d781687ddd567eb7e7bd
aurorastralis <41116388+aurorastralis@users.noreply.github.com>
2025-07-27 14:34:56 +0100

    Complete porfolio cards

--- javascript.js
+++ javascript.js
@@ -16,3 +16,13 @@ $(document).ready(function () {
     $grid.isotope({ filter: filterValue });
   });
 });
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function( event ) {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    var $button = $( event.currentTarget );
+    $button.addClass('is-checked');
+  });
+});
